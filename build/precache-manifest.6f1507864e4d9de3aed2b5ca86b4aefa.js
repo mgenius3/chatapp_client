6b35d5dc70826d67d9e6b70ec50d0a014e5ce502
Mosboy <benmos16@gmail.com>
2020-08-17 16:03:36 +0100

    ignoring some files

---
+++ build/precache-manifest.6f1507864e4d9de3aed2b5ca86b4aefa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d217cc0d3442b08203e45890a5de9cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a9077b6ceb917072c94",
+    "url": "/static/css/main.222ad576.chunk.css"
+  },
+  {
+    "revision": "f801557d4cccc7564dab",
+    "url": "/static/js/2.aba40f02.chunk.js"
+  },
+  {
+    "revision": "edd124757e19b78676b1e3bcc2fa9b0b",
+    "url": "/static/js/2.aba40f02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a9077b6ceb917072c94",
+    "url": "/static/js/main.1b88e1a1.chunk.js"
+  },
+  {
+    "revision": "a192ebfaab180aea100b",
+    "url": "/static/js/runtime-main.0826ba4c.js"
+  },
+  {
+    "revision": "c8c459d59c0923f07bafe3b16c7e193d",
+    "url": "/static/media/earth_ball.c8c459d5.jpg"
+  }
+]);
